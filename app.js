8427871db99d5d6243542c96c41ab02af1c84744
shrikanth <shrikanth22110402@snuchennai.edu.in>
2025-11-01 21:01:24 +0530

    Initial commit for NodeJS REST API

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'NodeJS API is running successfully!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
